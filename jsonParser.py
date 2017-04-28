77e24b70cea7e40743e3da26295166fb16bc95cd
Brenda Thayillam <bsthayillam@gmail.com>
2017-04-27 21:02:09 -0400

    Data parsing done

--- jsonParser.py
+++ jsonParser.py
@@ -1,6 +1,7 @@
 import json
 import random
-import pprint
+import re
+import sys
 
 with open('imdb_output.json') as data_file:
 	data = json.load(data_file)
@@ -11,20 +12,85 @@ with open('imdb_output.json') as data_file:
 
 # Use: 'gross', 'budget', 'num_facebook_like' --> 'imdb_score'
 
-keys_of_interest = ['gross', 'budget', 'num_facebook_like', 'imdb_score']
+keys_of_interest = ['gross', 'budget', 'num_voted_users', 'num_facebook_like','imdb_score']
 
-training = data
+training = []
+crossVal = []
 testing = []
+initN = len(data)
+
+minGross = sys.maxsize
+maxGross = 0
+minBudget = sys.maxsize
+maxBudget = 0
+minNumVoted = sys.maxsize
+maxNumVoted = 0
+minFBLikes = sys.maxsize
+maxFBLikes = 0
+minDirFBLikes = sys.maxsize
+maxDirFBLikes = 0
 
 # Filter out unnecessary keys
-for i in range(len(training)):
-	training[i] = {key: value for (key, value) in training[i].items() if (key in keys_of_interest)}
+count = 0
+for i in range(initN):
+	if (data[i]['director_info'] != None and data[i]['director_info']['director_facebook_likes'] != None and data[i]['director_info']['director_facebook_likes'] != "One"):
+		if (len(data[i]['gross']) > 0 and len(data[i]['budget']) > 0 and data[i]['num_facebook_like'] != None):
+			gross = int(re.sub("\D", "", data[i]['gross'][0]))
+			budget = int(re.sub("\D", "", data[i]['budget'][0]))
+			num_voted = data[i]['num_voted_users']
+			fb_likes = int(re.sub("\D", "", data[i]['num_facebook_like'])) * 1000
+			imdb_score = float(data[i]['imdb_score'][0])
+			dir_fb_likes = int(re.sub("\D", "", data[i]['director_info']['director_facebook_likes'])) * 1000
+			training = training + [{'gross' : gross, 'budget' : budget, 'num_voted_users' : num_voted, 'num_facebook_like' : fb_likes,
+									'director_facebook_likes' : dir_fb_likes, 'imdb_score' : imdb_score}]
+			if (gross < minGross):
+				minGross = gross
+			if (gross > maxGross):
+				maxGross = gross
+			if (budget < minBudget):
+				minBudget = budget
+			if (budget > maxBudget):
+				maxBudget = budget
+			if (num_voted < minNumVoted):
+				minNumVoted = num_voted
+			if (num_voted > maxNumVoted):
+				maxNumVoted = num_voted
+			if (fb_likes < minFBLikes):
+				minFBLikes = fb_likes
+			if (fb_likes > maxFBLikes):
+				maxFBLikes = fb_likes
+			if (dir_fb_likes < minDirFBLikes):
+				minDirFBLikes = dir_fb_likes
+			if (dir_fb_likes > maxDirFBLikes):
+				maxDirFBLikes = dir_fb_likes
+			count += 1
 
+# Normalize Data
+for i in range(len(training)):
+	gross = (training[i]['gross'] - minGross) / (maxGross - minGross)
+	budget = (training[i]['budget'] - minBudget) / (maxBudget - minBudget)
+	num_voted = (training[i]['num_voted_users'] - minNumVoted) / (maxNumVoted - minNumVoted)
+	fb_likes = (training[i]['num_facebook_like'] - minFBLikes) / (maxFBLikes - minFBLikes)
+	dir_fb_likes = (training[i]['director_facebook_likes'] - minDirFBLikes) / (maxDirFBLikes - minDirFBLikes)
+	training[i]['gross'] = gross
+	training[i]['budget'] = budget
+	training[i]['num_voted_users'] = num_voted
+	training[i]['num_facebook_like'] = fb_likes
+	training[i]['director_facebook_likes'] = dir_fb_likes
+
+# Split data into 500 testing set and remaining training set
 for i in range(500):
 	r = random.randint(0, len(training) - 1)
 	testing = testing + [training.pop(r)]
 
-print(len(training))
-print(training[0].keys())
-print(len(testing))
-print(testing[0].keys())
+# Split data into 500 cross validation set and remaining training set
+for i in range(500):
+	r = random.randint(0, len(training) - 1)
+	crossVal = crossVal + [training.pop(r)]
+
+# print(len(training))
+# print(training[0])
+# print(len(crossVal))
+# print(crossVal[0])
+# print(len(testing))
+# print(testing[0])
